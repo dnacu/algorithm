66c72f22517ecdfffc039bd1b5ed26992d63906e
dnacu <winteri1276@gmail.com>
2020-04-26 00:12:07 +0900

    .

---
+++ PROGRAMMERS/동적계획법(Dynamic Programming)/카드게임.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> left, vector<int> right) {
+    vector<vector<int>> dp (left.size()+1,vector<int>(right.size()+1));
+
+    for (int i = left.size() - 1; i >= 0; i--) 
+    {
+        for (int j = right.size() - 1; j >= 0; j--) 
+        {
+            if (left[i] > right[j]) 
+                dp[i][j] = dp[i][j + 1] + right[j];
+            else 
+                dp[i][j] = max(dp[i + 1][j], dp[i + 1][j + 1]); 
+        }
+    }
+    return dp[0][0];
+}
