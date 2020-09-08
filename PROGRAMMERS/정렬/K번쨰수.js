855b86e01c297330204a7c5a18f3d44ff1eb0fda
dnacu <winteri1276@gmail.com>
2020-09-08 23:34:54 +0900

    정렬

--- PROGRAMMERS/정렬/K번쨰수.js
+++ PROGRAMMERS/정렬/K번쨰수.js
@@ -1,11 +1,7 @@
 function solution(array, commands) {
-  var answer = [];
-  
-  commands.map(c => {
-      const [i, j, k] = c;
-      const tmp = array.slice(i-1, j).sort((a,b) => a-b);
-      answer.push(tmp[k-1]);
-  })
-  
-  return answer;
+  return commands.map((c) => {
+    const [i, j, k] = c;
+    // compareFunction이 제공되지 않으면 요소를 문자열로 변환하고 유니 코드 코드 포인트 순서로 문자열을 비교하여 정렬됨
+    return array.slice(i - 1, j).sort((a, b) => a - b)[k - 1];
+  });
 }
